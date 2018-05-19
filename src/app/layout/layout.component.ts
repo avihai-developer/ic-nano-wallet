85641ce27eddd9dc51d86af33c47f46346c8897f
Avihai Cohen <avihai.android@gmail.com>
2018-05-19 13:56:18 +0300

    side nav

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-layout',
-  templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+    selector: 'app-layout',
+    templateUrl: './layout.component.html',
+    styleUrls: ['./layout.component.scss']
 })
+
+
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+    isMobile: boolean;
+    sideNavMode: string;
+    isSideBarOpen: boolean;
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.handleSideBar();
+    }
+
+    ngOnInit() {
+    }
+
+    @HostListener('window:resize')
+    onResize() {
+        this.handleSideBar();
+    }
+
+    handleSideBar() {
+        if (window.innerWidth <= 480) {
+            this.isMobile = true;
+            this.sideNavMode = 'over';
+            this.isSideBarOpen = false;
+        } else {
+            this.isMobile = false;
+            this.sideNavMode = 'side';
+            this.isSideBarOpen = true;
+        }
+    }
 
 }
